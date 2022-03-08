76a1326cd775ddac08bc600d322afa7473bd4ad2
thienjs <thien.tran6@gmail.com>
2022-03-08 16:07:10 -0500

    pages

---
+++ pages/play.tsx
@@ -0,0 +1,22 @@
+import AppLayout from "@lib/components/Layouts/AppLayout";
+
+const Play = () => {
+  return (
+    <>
+      <AppLayout>
+        {/* <blockquote> */}
+        <h1>Play</h1>
+        <p>
+         Social App for tennis players
+        </p>
+        <blockquote>
+          <p>  
+           meet, chat, learn with other players
+          </p>
+        </blockquote>
+      </AppLayout>
+    </>
+  );
+};
+
+export default Play;
